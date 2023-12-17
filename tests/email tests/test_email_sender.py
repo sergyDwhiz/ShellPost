7c0f034a79407a4e4bb2feccb932fdb1fed10557
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-17 18:35:45 +0100

    Implement unit test for send_email function

--- tests/email tests/test_email_sender.py
+++ tests/email tests/test_email_sender.py
@@ -1,1 +1,19 @@
 # Unit tests for "email_sender.py" script
+
+import unittest
+from unittest.mock import patch, MagicMock
+from src.email.email_sender import email_sender
+
+class TestEmailSender(unittest.TestCase):
+    @patch('src.email.email_sender.smtplib.SMTP')
+    @patch('src.email.email_sender.getpass.getpass')
+    @patch('builtins.input')
+    def test_send_email(self, mock_input, mock_getpass, mock_smtp):
+        # Arrange
+        mock_input.return_value = 'test_username'
+        mock_getpass.return_value = 'test_password'
+        mock_server = MagicMock()
+        mock_smtp.return_value = mock_server
+        attachment_path = 'attachment_path'
+
+        
