59fe53dbc194dc26468f2ebd4961c8cec131b27d
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-17 18:37:08 +0100

    Run tests if file is direclty executed

--- tests/email tests/test_email_sender.py
+++ tests/email tests/test_email_sender.py
@@ -15,5 +15,16 @@ class TestEmailSender(unittest.TestCase):
         mock_server = MagicMock()
         mock_smtp.return_value = mock_server
         attachment_path = 'attachment_path'
+   
+        # Act 
+        email_sender.send_email(attachment_path)
 
+        # Assert
+        mock_smtp.assert_called_once_with(email_sender.server, email_sender.port)
+        mock_server.starttls.assert_called_once()
+        mock_server.login.assert_called_once_with('test_username', 'test_password')
+        mock_server.sendmail.assert_called_once_with(email_sender.sender, email_sender.recipient, ANY)
+        mock_server.quit.assert_called_once()
         
+if __name__ == '__main__': # Run tests if file directly executed
+    unittest.main() 
