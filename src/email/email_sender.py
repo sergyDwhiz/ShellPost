bcb984d83b0c797fa66b2d17b5b4c01e8fa7bb62
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-25 21:55:12 +0100

    Add main file docstring for easy comprehension

--- src/email/email_sender.py
+++ src/email/email_sender.py
@@ -1,15 +1,15 @@
+import settings
 '''
 Main Code Description: This piece of software is simply a python script 
 that sends emails to a desired recipient with the option of including an
 attachment via the Command line/terminal. 
 
 Copyright: (C) Sergius Nyah 2023-12-17
-License: MIT License
- '''
+'''
 
 # Import the settings from the settings.py file
 # Contains the configuration settings for the email sender. 
-import settings
+import settings.py 
 
 # Use the settings in your email sending code
 server = settings.EMAIL_HOST
